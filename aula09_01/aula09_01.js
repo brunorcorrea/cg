2b7a2ba12a0f127cfc15892d2e49986d352fe006
Bruno Correa <correaricardobruno@gmail.com>
2024-05-02 21:44:32 -0300

    Utilizando matriz escalar

--- aula09_01/aula09_01.js
+++ aula09_01/aula09_01.js
@@ -55,7 +55,7 @@ utils.initShader({
 
     uniform vec3 translation;
     uniform vec3 theta;
-    uniform float scalarValue;
+    uniform vec3 scalar;
 
     void main(){
       vec3 angles = radians(theta);
@@ -83,11 +83,17 @@ utils.initShader({
         0.0, 0.0, 1.0, 0.0,
         translation.x, translation.y, translation.z, 1.0);
 
-      gl_PointSize = 10.0;
-      vec4 rotatedPosition = rz * ry * rx * vec4(aPosition, 1.0);
-      vec4 translatedPosition = translationMatrix * rotatedPosition;
-      gl_Position = vec4(scalarValue * translatedPosition.xyz, 1.0);
-      vColor = vec4(aColor, 1.0);
+        mat4 scalarMatrix = mat4(
+          scalar.x, 0.0, 0.0, 0.0,
+          0.0, scalar.y, 0.0, 0.0,
+          0.0, 0.0, scalar.z, 0.0,
+          0.0, 0.0, 0.0, 1.0);
+  
+        gl_PointSize = 10.0;
+        vec4 rotatedPosition = rz * ry * rx * vec4(aPosition, 1.0);
+        vec4 translatedPosition = translationMatrix * rotatedPosition;
+        gl_Position = scalarMatrix * translatedPosition;
+        vColor = vec4(aColor, 1.0);
     }`,
   fragmentShader: `#version 300 es
       precision highp float;
@@ -105,9 +111,9 @@ utils.initBuffer({ vertices: colors });
 utils.linkBuffer({ variable: "aColor", reading: 3 });
 
 var theta = [0, 0, 0];
-var translation = [0,0,0];
-var scale = 0;
-var scalarValue = 1;
+var translation = [0, 0, 0];
+var scale = [0, 0, 0];
+var scalar = [1, 1, 1];
 var transform_x = 0;
 var transform_y = 0;
 var transform_z = 0;
@@ -120,11 +126,12 @@ var timeoutId;
 
 function changeSpeed(newSpeed) {
   speed = 100 - newSpeed;
-  if (speed == 100) { //Considera-se que a speed 100 é parado e 0 é mais rápido
-    clearTimeout(timeoutId)
-    timeoutId = -1
-  } else if(timeoutId == -1) {
-    timeoutId = blabla()
+  if (speed == 100) {
+    //Considera-se que a speed 100 é parado e 0 é mais rápido
+    clearTimeout(timeoutId);
+    timeoutId = -1;
+  } else if (timeoutId == -1) {
+    timeoutId = blabla();
   }
 }
 
@@ -169,15 +176,21 @@ function changeDirectionRotateZ() {
 }
 
 function startScale() {
-  scale += 0.01;
+  scale[0] += 0.01;
+  scale[1] += 0.01;
+  scale[2] += 0.01;
 }
 
 function stopScale() {
-  scale = 0;
+  scale[0] = 0;
+  scale[1] = 0;
+  scale[2] = 0;
 }
 
 function changeDirectionScale() {
-  scale *= -1;
+  scale[0] *= -1;
+  scale[1] *= -1;
+  scale[2] *= -1;
 }
 
 function startTranslateX() {
@@ -224,8 +237,9 @@ function render() {
   });
 
   utils.linkUniformVariable({
-    shaderName: "scalarValue",
-    value: scalarValue,
+    shaderName: "scalar",
+    value: scalar,
+    kind: "3fv",
   });
 
   utils.linkUniformVariable({
@@ -236,7 +250,9 @@ function render() {
 
   utils.drawElements({ method: "TRIANGLES" });
 
-  scalarValue += scale;
+  scalar[0] += scale[0];
+  scalar[1] += scale[1];
+  scalar[2] += scale[2];
   theta[0] += transform_x;
   theta[1] += transform_y;
   theta[2] += transform_z;
@@ -249,36 +265,74 @@ function render() {
 
 render();
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("slider").addEventListener("input", function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("slider").addEventListener("input", function (event) {
     changeSpeed(event.target.value);
   });
 
-  document.getElementById("RotationX").addEventListener("click", changeDirectionRotateX);
-  document.getElementById("RotationStartX").addEventListener("click", startRotateX);
-  document.getElementById("RotationStopX").addEventListener("click", stopRotateX);
+  document
+    .getElementById("RotationX")
+    .addEventListener("click", changeDirectionRotateX);
+  document
+    .getElementById("RotationStartX")
+    .addEventListener("click", startRotateX);
+  document
+    .getElementById("RotationStopX")
+    .addEventListener("click", stopRotateX);
 
-  document.getElementById("RotationY").addEventListener("click", changeDirectionRotateY);
-  document.getElementById("RotationStartY").addEventListener("click", startRotateY);
-  document.getElementById("RotationStopY").addEventListener("click", stopRotateY);
+  document
+    .getElementById("RotationY")
+    .addEventListener("click", changeDirectionRotateY);
+  document
+    .getElementById("RotationStartY")
+    .addEventListener("click", startRotateY);
+  document
+    .getElementById("RotationStopY")
+    .addEventListener("click", stopRotateY);
 
-  document.getElementById("RotationZ").addEventListener("click", changeDirectionRotateZ);
-  document.getElementById("RotationStartZ").addEventListener("click", startRotateZ);
-  document.getElementById("RotationStopZ").addEventListener("click", stopRotateZ);
+  document
+    .getElementById("RotationZ")
+    .addEventListener("click", changeDirectionRotateZ);
+  document
+    .getElementById("RotationStartZ")
+    .addEventListener("click", startRotateZ);
+  document
+    .getElementById("RotationStopZ")
+    .addEventListener("click", stopRotateZ);
 
-  document.getElementById("ScaleDirection").addEventListener("click", changeDirectionScale);
+  document
+    .getElementById("ScaleDirection")
+    .addEventListener("click", changeDirectionScale);
   document.getElementById("ScaleStart").addEventListener("click", startScale);
   document.getElementById("ScaleStop").addEventListener("click", stopScale);
 
-  document.getElementById("TranslationStartX").addEventListener("click", startTranslateX);
-  document.getElementById("TranslationStopX").addEventListener("click", stopTranslateX);
-  document.getElementById("TranslationX").addEventListener("click", changeDirectionTranslateX);
+  document
+    .getElementById("TranslationStartX")
+    .addEventListener("click", startTranslateX);
+  document
+    .getElementById("TranslationStopX")
+    .addEventListener("click", stopTranslateX);
+  document
+    .getElementById("TranslationX")
+    .addEventListener("click", changeDirectionTranslateX);
 
-  document.getElementById("TranslationStartY").addEventListener("click", startTranslateY);
-  document.getElementById("TranslationStopY").addEventListener("click", stopTranslateY);
-  document.getElementById("TranslationY").addEventListener("click", changeDirectionTranslateY);
+  document
+    .getElementById("TranslationStartY")
+    .addEventListener("click", startTranslateY);
+  document
+    .getElementById("TranslationStopY")
+    .addEventListener("click", stopTranslateY);
+  document
+    .getElementById("TranslationY")
+    .addEventListener("click", changeDirectionTranslateY);
 
-  document.getElementById("TranslationStartZ").addEventListener("click", startTranslateZ);
-  document.getElementById("TranslationStopZ").addEventListener("click", stopTranslateZ);
-  document.getElementById("TranslationZ").addEventListener("click", changeDirectionTranslateZ);
+  document
+    .getElementById("TranslationStartZ")
+    .addEventListener("click", startTranslateZ);
+  document
+    .getElementById("TranslationStopZ")
+    .addEventListener("click", stopTranslateZ);
+  document
+    .getElementById("TranslationZ")
+    .addEventListener("click", changeDirectionTranslateZ);
 });
