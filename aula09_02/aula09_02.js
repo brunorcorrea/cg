a6a0c0a1ffdd3114500e77a9e678df69b3ae2cdf
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2024-05-11 12:29:39 -0300

    fix file names

--- aula09_02/aula09_02.js
+++ aula09_02/aula09_02.js
@@ -1,8 +1,7 @@
-var utils = new Utils();
+var utils = new Utils(300, 200, 255, 0.1, 0.2, 0.5, 0.5);
 
 var vertices = [];
 var colors = [];
-var normals = [];
 
 var cubeVertices = [
   [-0.5, -0.5, 0.5],
@@ -14,177 +13,326 @@ var cubeVertices = [
   [0.5, 0.5, -0.5],
   [0.5, -0.5, -0.5],
 ];
-
 var cubeColors = [
-  [0.0, 0.0, 0.0], // preto
-  [1.0, 0.0, 0.0], // vermelho
-  [1.0, 1.0, 0.0], // amarelo
-  [0.0, 1.0, 0.0], // verde
-  [0.0, 0.0, 1.0], // azul
-  [1.0, 0.0, 1.0], // rosa
-  [0.0, 1.0, 1.0], // ciano
-  [1.0, 1.0, 1.0], // branco
+  [1.0, 0.0, 0.0],
+  [0.0, 1.0, 0.0],
+  [0.0, 0.0, 1.0],
+  [1.0, 1.0, 0.0],
+  [1.0, 0.0, 1.0],
+  [0.0, 1.0, 1.0],
+  [0.5, 0.5, 0.5],
+  [0.3, 0.7, 0.2],
 ];
 
-function computeProdutoVetorial(A, B, C) {
-    let AB = [B[0] - A[0], B[1] - A[1], B[2] - A[2]];
-    let AC = [C[0] - A[0], C[1] - A[1], C[2] - A[2]];
-  
-    let Nx = AB[1] * AC[2] - AB[2] * AC[1];
-    let Ny = AB[2] * AC[0] - AB[0] * AC[2];
-    let Nz = AB[0] * AC[1] - AB[1] * AC[0];
-  
-    return [Nx, Ny, Nz];
-  }
-  
+const makeFace = (v1, v2, v3, v4) => {
+  var triangles = [v1, v2, v3, v4, v3, v1];
 
-function makeFace(v1, v2, v3, v4) {
-  triangulos = [v1, v2, v3, v1, v3, v4];
+  triangles.forEach((triangle) => {
+    cubeColors[v1].forEach((color) => {
+      colors.push(color);
+    });
 
-  let normal = computeProdutoVetorial(cubeVertices[v1], cubeVertices[v2], cubeVertices[v3]);
+    cubeVertices[triangle].forEach((vertice) => {
+      vertices.push(vertice);
+    });
+  });
+};
 
-  for (var i = 0; i < triangulos.length; i++) {
-    vertices.push(cubeVertices[triangulos[i]][0]);
-    vertices.push(cubeVertices[triangulos[i]][1]);
-    vertices.push(cubeVertices[triangulos[i]][2]);
-
-    colors.push(cubeColors[v1][0]);
-    colors.push(cubeColors[v1][1]);
-    colors.push(cubeColors[v1][2]);
-
-    normals.push(normal[0]);
-    normals.push(normal[1]);
-    normals.push(normal[2]);
-  }
-}
-
+makeFace(0, 1, 5, 4);
 makeFace(1, 0, 3, 2);
 makeFace(2, 3, 7, 6);
 makeFace(3, 0, 4, 7);
-makeFace(6, 5, 1, 2);
 makeFace(4, 5, 6, 7);
-makeFace(5, 4, 0, 1);
+makeFace(6, 5, 1, 2);
 
 utils.initShader({
   vertexShader: `#version 300 es
-precision mediump float;
+    precision mediump float;
 
-in vec3 aPosition;
-in vec3 aColor;
-in vec3 aNormal;
+    in vec3 aPosition;
+    in vec3 aColor;
+    out vec4 vColor;
 
-out vec4 vColor;
+    uniform vec3 translation;
+    uniform vec3 theta;
+    uniform vec3 scalar;
 
-uniform vec3 theta;
+    void main(){
+      vec3 angles = radians(theta);
+      vec3 c = cos(angles);
+      vec3 s = sin(angles);
 
-uniform vec3 uAmbientLight;
-uniform vec3 uLightColor;
-uniform vec3 uLightDirection;
+      mat4 rx = mat4(1.0, 0.0, 0.0, 0.0,
+        0.0, c.x, s.x, 0.0,
+        0.0, -s.x, c.x, 0.0,
+        0.0, 0.0, 0.0, 1.0);
+  
+      mat4 ry = mat4(c.y, 0.0, -s.y, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        s.y, 0.0, c.y, 0.0,
+        0.0, 0.0, 0.0, 1.0);
+  
+      mat4 rz = mat4(c.z, s.z, 0.0, 0.0,
+        -s.z, c.z, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0);
 
-void main(){
-   // Computando seno e cosseno para cada um dos três eixos;
-   vec3 angles = radians(theta);
-   vec3 c = cos(angles);
-   vec3 s = sin(angles);
+      mat4 translationMatrix = mat4(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        translation.x, translation.y, translation.z, 1.0);
 
-   // Matrizes de rotação;
-   mat4 rx = mat4( 1.0,  0.0,  0.0, 0.0,
-                   0.0,  c.x,  s.x, 0.0,
-                   0.0, -s.x,  c.x, 0.0,
-                   0.0,  0.0,  0.0, 1.0);
-
-   mat4 ry = mat4( c.y, 0.0, -s.y, 0.0,
-                   0.0, 1.0,  0.0, 0.0,
-                   s.y, 0.0,  c.y, 0.0,
-                   0.0, 0.0,  0.0, 1.0);
-
-   mat4 rz = mat4( c.z, s.z, 0.0, 0.0,
-                   -s.z,  c.z, 0.0, 0.0,
-                    0.0,  0.0, 1.0, 0.0,
-                    0.0,  0.0, 0.0, 1.0);
-
-   mat4 modelMatrix =  rz * ry * rx;
-   gl_Position = modelMatrix * vec4(aPosition, 1.0);
-
-   vec3 transformedNormal = mat3(modelMatrix) * aNormal;
-   vec3 normal = normalize(transformedNormal);
-   float diff = max(dot(normal, -normalize(uLightDirection)), 0.0);
-   vec3 diffuse = uLightColor * diff;
-   vec3 ambient = uAmbientLight;
-   vec3 resultColor = ambient + diffuse;
-
-   vColor = vec4(resultColor * aColor, 1.0);
-}`,
+        mat4 scalarMatrix = mat4(
+          scalar.x, 0.0, 0.0, 0.0,
+          0.0, scalar.y, 0.0, 0.0,
+          0.0, 0.0, scalar.z, 0.0,
+          0.0, 0.0, 0.0, 1.0);
+  
+        gl_PointSize = 10.0;
+        vec4 rotatedPosition = rz * ry * rx * vec4(aPosition, 1.0);
+        vec4 translatedPosition = translationMatrix * rotatedPosition;
+        gl_Position = scalarMatrix * translatedPosition;
+        vColor = vec4(aColor, 1.0);
+    }`,
   fragmentShader: `#version 300 es
-precision mediump float;
-
-in vec4 vColor;
-out vec4 fColor;
-
-void main() {
-    fColor = vColor;
-}`,
+      precision highp float;
+      in vec4 vColor;
+      out vec4 fColor;
+      void main(){
+        fColor = vColor;
+      }`,
 });
 
-utils.initBuffer({ vertices });
-utils.linkBuffer({ reading: 3 });
+utils.initBuffer({ vertices: vertices });
+utils.linkBuffer({ variable: "aPosition", reading: 3 });
 
 utils.initBuffer({ vertices: colors });
 utils.linkBuffer({ variable: "aColor", reading: 3 });
 
-utils.initBuffer({ vertices: colors });
-utils.linkBuffer({ variable: "aColor", reading: 3 });
-
-utils.initBuffer({ vertices: normals });
-utils.linkBuffer({ variable: "aNormal", reading: 3 });
-
-var theta = [0.0, 0.0, 0.0];
-
-var rotation_x = 1.0;
-var rotation_y = 4.0;
-var rotation_z = 0.0;
-
+var theta = [0, 0, 0];
+var translation = [0, 0, 0];
+var scale = [0, 0, 0];
+var scalar = [1, 1, 1];
+var transform_x = 0;
+var transform_y = 0;
+var transform_z = 0;
+var translation_x = 0;
+var translation_y = 0;
+var translation_z = 0;
 var speed = 100;
 
-var uAmbientLight = [0.2, 0.2, 0.2]; // Luz ambiente fraca
-var uLightColor = [1, 1, 1]; // Cor da luz
-var uLightDirection = [1, 1, 1]; // Posição
+var timeoutId;
 
-function normalizeVector(vec) {
-  var len = Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
-  vec[0] /= len;
-  vec[1] /= len;
-  vec[2] /= len;
+function changeSpeed(newSpeed) {
+  speed = 100 - newSpeed;
+  if (speed == 100) {
+    //Considera-se que a speed 100 é parado e 0 é mais rápido
+    clearTimeout(timeoutId);
+    timeoutId = -1;
+  } else if (timeoutId == -1) {
+    timeoutId = blabla();
+  }
 }
-normalizeVector(uLightDirection);
 
-utils.linkUniformVariable({
-  shaderName: "uAmbientLight",
-  value: uAmbientLight,
-  kind: "3fv",
-});
+function blabla() {
+  return setTimeout(render, speed);
+}
 
-utils.linkUniformVariable({
-    shaderName: "uLightColor",
-    value: uLightColor,
+function startRotateX() {
+  transform_x = 1;
+}
+
+function stopRotateX() {
+  transform_x = 0;
+}
+
+function changeDirectionRotateX() {
+  transform_x *= -1;
+}
+
+function startRotateY() {
+  transform_y = 1;
+}
+
+function stopRotateY() {
+  transform_y = 0;
+}
+
+function changeDirectionRotateY() {
+  transform_y *= -1;
+}
+
+function startRotateZ() {
+  transform_z = 1;
+}
+
+function stopRotateZ() {
+  transform_z = 0;
+}
+
+function changeDirectionRotateZ() {
+  transform_z *= -1;
+}
+
+function startScale() {
+  scale[0] += 0.01;
+  scale[1] += 0.01;
+  scale[2] += 0.01;
+}
+
+function stopScale() {
+  scale[0] = 0;
+  scale[1] = 0;
+  scale[2] = 0;
+}
+
+function changeDirectionScale() {
+  scale[0] *= -1;
+  scale[1] *= -1;
+  scale[2] *= -1;
+}
+
+function startTranslateX() {
+  translation_x = 0.01;
+}
+
+function stopTranslateX() {
+  translation_x = 0;
+}
+
+function changeDirectionTranslateX() {
+  translation_x *= -1;
+}
+
+function startTranslateY() {
+  translation_y = 0.01;
+}
+
+function stopTranslateY() {
+  translation_y = 0;
+}
+
+function changeDirectionTranslateY() {
+  translation_y *= -1;
+}
+
+function startTranslateZ() {
+  translation_z = 0.01;
+}
+
+function stopTranslateZ() {
+  translation_z = 0;
+}
+
+function changeDirectionTranslateZ() {
+  translation_z *= -1;
+}
+
+function render() {
+  utils.linkUniformVariable({
+    shaderName: "theta",
+    value: theta,
     kind: "3fv",
   });
 
-utils.linkUniformVariable({
-shaderName: "uLightDirection",
-value: uLightDirection,
-kind: "3fv",
-});
+  utils.linkUniformVariable({
+    shaderName: "scalar",
+    value: scalar,
+    kind: "3fv",
+  });
 
-function render() {
-  theta[0] += rotation_x;
-  theta[1] += rotation_y;
-  theta[2] += rotation_z;
-
-  utils.linkUniformVariable({ shaderName: "theta", value: theta, kind: "3fv" });
+  utils.linkUniformVariable({
+    shaderName: "translation",
+    value: translation,
+    kind: "3fv",
+  });
 
   utils.drawElements({ method: "TRIANGLES" });
 
-  setTimeout(render, speed);
+  scalar[0] += scale[0];
+  scalar[1] += scale[1];
+  scalar[2] += scale[2];
+  theta[0] += transform_x;
+  theta[1] += transform_y;
+  theta[2] += transform_z;
+  translation[0] += translation_x;
+  translation[1] += translation_y;
+  translation[2] += translation_z;
+
+  timeoutId = blabla();
 }
+
 render();
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("slider").addEventListener("input", function (event) {
+    changeSpeed(event.target.value);
+  });
+
+  document
+    .getElementById("RotationX")
+    .addEventListener("click", changeDirectionRotateX);
+  document
+    .getElementById("RotationStartX")
+    .addEventListener("click", startRotateX);
+  document
+    .getElementById("RotationStopX")
+    .addEventListener("click", stopRotateX);
+
+  document
+    .getElementById("RotationY")
+    .addEventListener("click", changeDirectionRotateY);
+  document
+    .getElementById("RotationStartY")
+    .addEventListener("click", startRotateY);
+  document
+    .getElementById("RotationStopY")
+    .addEventListener("click", stopRotateY);
+
+  document
+    .getElementById("RotationZ")
+    .addEventListener("click", changeDirectionRotateZ);
+  document
+    .getElementById("RotationStartZ")
+    .addEventListener("click", startRotateZ);
+  document
+    .getElementById("RotationStopZ")
+    .addEventListener("click", stopRotateZ);
+
+  document
+    .getElementById("ScaleDirection")
+    .addEventListener("click", changeDirectionScale);
+  document.getElementById("ScaleStart").addEventListener("click", startScale);
+  document.getElementById("ScaleStop").addEventListener("click", stopScale);
+
+  document
+    .getElementById("TranslationStartX")
+    .addEventListener("click", startTranslateX);
+  document
+    .getElementById("TranslationStopX")
+    .addEventListener("click", stopTranslateX);
+  document
+    .getElementById("TranslationX")
+    .addEventListener("click", changeDirectionTranslateX);
+
+  document
+    .getElementById("TranslationStartY")
+    .addEventListener("click", startTranslateY);
+  document
+    .getElementById("TranslationStopY")
+    .addEventListener("click", stopTranslateY);
+  document
+    .getElementById("TranslationY")
+    .addEventListener("click", changeDirectionTranslateY);
+
+  document
+    .getElementById("TranslationStartZ")
+    .addEventListener("click", startTranslateZ);
+  document
+    .getElementById("TranslationStopZ")
+    .addEventListener("click", stopTranslateZ);
+  document
+    .getElementById("TranslationZ")
+    .addEventListener("click", changeDirectionTranslateZ);
+});
